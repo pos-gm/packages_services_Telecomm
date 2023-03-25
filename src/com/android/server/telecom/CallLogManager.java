e2b563e4f370606ceac043d150c4cb82dc577feb
basamaryan <basam.aryan@gmail.com>
2023-03-25 21:31:08 +0000

    Revert "Telecom: Add sensitive phone numbers hooks"
    This reverts commit 17893e07c11e8cf735605fd52162a0c7c8e7c34f.

--- src/com/android/server/telecom/CallLogManager.java
+++ src/com/android/server/telecom/CallLogManager.java
@@ -46,8 +46,6 @@ import android.telephony.SubscriptionManager;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.callfiltering.CallFilteringResult;
 
-import org.lineageos.lib.phone.SensitivePhoneNumbers;
-
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
@@ -104,7 +102,6 @@ public final class CallLogManager extends CallsManagerListenerBase {
 
     private Object mLock;
     private String mCurrentCountryIso;
-    private SensitivePhoneNumbers mSensitivePhoneNumbers;
 
     public CallLogManager(Context context, PhoneAccountRegistrar phoneAccountRegistrar,
             MissedCallNotifier missedCallNotifier) {
@@ -114,7 +111,6 @@ public final class CallLogManager extends CallsManagerListenerBase {
         mPhoneAccountRegistrar = phoneAccountRegistrar;
         mMissedCallNotifier = missedCallNotifier;
         mLock = new Object();
-        mSensitivePhoneNumbers = SensitivePhoneNumbers.getInstance();
     }
 
     @Override
@@ -401,20 +397,16 @@ public final class CallLogManager extends CallsManagerListenerBase {
         boolean okToLogEmergencyNumber = false;
         CarrierConfigManager configManager = (CarrierConfigManager) mContext.getSystemService(
                 Context.CARRIER_CONFIG_SERVICE);
-        int subId = mPhoneAccountRegistrar.getSubscriptionIdForPhoneAccount(accountHandle);
-        PersistableBundle configBundle = configManager.getConfigForSubId(subId);
+        PersistableBundle configBundle = configManager.getConfigForSubId(
+                mPhoneAccountRegistrar.getSubscriptionIdForPhoneAccount(accountHandle));
         if (configBundle != null) {
             okToLogEmergencyNumber = configBundle.getBoolean(
                     CarrierConfigManager.KEY_ALLOW_EMERGENCY_NUMBERS_IN_CALL_LOG_BOOL);
         }
 
-        // Don't log sensitive numbers.
-        boolean isSensitiveNumber = mSensitivePhoneNumbers.isSensitiveNumber(mContext, number,
-                subId);
-
         // Don't log emergency numbers if the device doesn't allow it.
         return (!isEmergency || okToLogEmergencyNumber)
-                && !isUnloggableNumber(number, configBundle) && !isSensitiveNumber;
+                && !isUnloggableNumber(number, configBundle);
     }
 
     private boolean isUnloggableNumber(String callNumber, PersistableBundle carrierConfig) {
